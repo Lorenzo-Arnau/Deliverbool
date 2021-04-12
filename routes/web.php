f75daa0c7dc48a283d0f0eb2fdb245dabfad94cf
Lorenzo-Arnau <skyline03@hotmail.it>
2021-04-12 17:19:35 +0200

    Update, Routes

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('edo');
+});
+Route::get('/payment', function () {
     return view('welcome');
 });
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
